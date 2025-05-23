b702ea26308e1c9505bdfdceb0ab7fc3cb3fb909
JOAO FELIPE GONCALVES PASSOS <joaofelipe.gp96@gmail.com>
2025-05-23 00:59:27 +0000

    att2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 int main() {
-
     char Cidade[20];
     int População;
     float AreaKM;
     float PIB;
     int PontosTuristicos;
 
-printf("Cidade: \n");
-scanf("%s", Cidade);
+    printf("Cidade: \n");
+    scanf("%s", Cidade);
 
-printf("Populução: \n");
-scanf("%s", População);
+    printf("População: \n");
+    scanf("%d", &População);
 
-printf("ÁreaKM: \n");
-scanf("%f", AreaKM);
+    printf("ÁreaKM: \n");
+    scanf("%f", &AreaKM);
 
-printf("PIB: \n");
-scanf("%f", PIB);
+    printf("PIB: \n");
+    scanf("%f", &PIB);
 
-printf("PontosTurísticos: \n");
-scanf("%s", PontosTuristicos);
+    printf("PontosTurísticos: \n");
+    scanf("%d", &PontosTuristicos);
 
     return 0;
 }
+
