f715f9449ac103634f60a1ac7f27cd3e1a08cd2f
JOAO FELIPE GONCALVES PASSOS <joaofelipe.gp96@gmail.com>
2025-05-23 00:01:16 +0000

    carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-
 int main() {
+    
     int Carta_1;
     int Cidade[20];
     int População;
@@ -29,14 +27,6 @@ scanf("%f", PIB);
 
 printf("PontosTurísticos: \n");
 scanf("%s", PontosTuristicos);
-
-
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
